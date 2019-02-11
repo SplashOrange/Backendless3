aa018d961fd48fde53a9f2d07cdec8f24fe2f58f
Hannah Johnson <hannah135johnson@yahoo.com>
2019-02-11 17:14:19 -0600

    Wrote a "test" comment

--- app/src/main/java/com/example/hjoh2002/backendless3/MainActivity.java
+++ app/src/main/java/com/example/hjoh2002/backendless3/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.hjoh2002.backendless3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
